1efa7cdc2570e9ef6e30a848bbe5049a0bd99dbb
Uri <43704209+uri-99@users.noreply.github.com>
2024-03-09 18:23:49 -0300

    feat: add auto_rfcat.sh

---
+++ auto_rfcat.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/expect -f
+
+spawn rfcat -r
+
+expect "(InteractiveConsole)\r\n>>> "
+
+send "d.setFreq(433920000)\n"
+expect ">>> "
+
+send "d.discover()\n"
+expect "(Press Enter to quit)\r"
+
+
+sleep 10
+
+send "\r"
+expect "Exiting Discover mode...\r\n>>> "
+#expect ">>> "
+send "exit()\n"
+expect "====="
+
+interact #this command hands control to the user
+
+#wait #this command waits for the process to end?
+

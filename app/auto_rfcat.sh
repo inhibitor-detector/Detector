b11e326dd1b357ce92ec9cd4ede55551d9bcde26
Urix <43704209+uri-99@users.noreply.github.com>
2024-04-14 19:41:53 -0300

    fix: auto_rfcat can wait for n seconds (it waited only for 10 due to timeout variable)

--- app/auto_rfcat.sh
+++ app/auto_rfcat.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/expect -f
 
+set timeout -1
+
 spawn rfcat -r
 
 expect "(InteractiveConsole)\r\n>>> "
@@ -10,12 +12,20 @@ expect ">>> "
 send "d.discover()\n"
 expect "(Press Enter to quit)\r"
 
+# TODO
+# if SLEEP var in .env is a number, wait those seconds, else wait indefinetly by doing expect "string that will not appear"
+# to wait indefinetly we could also experiment with the "wait" command,
+# if {[string match "some_condition" $expect_out(buffer)]} {
+#     send "do_something\r"
+# } else {
+#     send "do_something_else\r"
+# }
 
-sleep 10
+sleep 60
 
 send "\r"
 expect "Exiting Discover mode...\r\n>>> "
-#expect ">>> "
+
 send "exit()\n"
 expect "====="
 

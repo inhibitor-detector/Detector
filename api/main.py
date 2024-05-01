e9eb5d8d456c0ab8326f358605f5485829004ef8
Urix <43704209+uri-99@users.noreply.github.com>
2024-05-01 13:50:43 -0300

    fix: dont start when finished setup.sh

--- api/main.py
+++ api/main.py
@@ -6,6 +6,8 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.post("")
+
 # @app.get("/items/{item_id}")
 # def read_item(item_id: int, query_param: str = None):
 #     return {"item_id": item_id, "query_param": query_param}
--- setup.sh
+++ setup.sh
@@ -10,7 +10,6 @@ pip install --upgrade pip
 # Install expect tool
 apt-get install -y expect
 
-
 # Install rfcat
 git clone https://github.com/atlas0fd00m/rfcat.git
 cd rfcat
@@ -18,12 +17,8 @@ python3 setup.py install
 
 cd .. #return to /Tesis/app
 
-
 apt-get autoremove -y
 apt-get clean
 # worked in docker, check in .sh
 # rm -rf /var/lib/apt/lists/*
 # rm -rf /tmp/* /var/tmp/*
-
-# Run rfcat on start
-./start.sh

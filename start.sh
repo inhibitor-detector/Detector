b67729b0833a3c4c0ee4f1e60cea58dda1102836
Urix <43704209+uri-99@users.noreply.github.com>
2024-05-01 15:25:43 -0300

    refactor: move start to root dir

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-filename="./logs/log_$(date +'%Y-%m-%d_%H-%M-%S').txt"
+filename="./app/logs/log_$(date +'%Y-%m-%d_%H-%M-%S').txt"
 touch "$filename"
 
 # "&" is to run in parallel
 #tee allows to write to a file but also show in STO
 # ./auto_rfcat.sh | tee "$filename" &
-./auto_rfcat.sh >> "$filename" &
+./app/auto_rfcat.sh >> "$filename" &
 
-./analyzer.sh $filename &
+./app/analyzer.sh $filename &
 
 wait
 

d662c26f72f7acb620939e91c8737094cbbbb059
Urix <43704209+uri-99@users.noreply.github.com>
2024-05-12 20:02:57 -0300

    feat: beep in loop

--- app/src/alarm_test.py
+++ app/src/alarm_test.py
@@ -6,11 +6,10 @@ pin_number = 1
 
 # Set up GPIO
 GPIO.setmode(GPIO.BCM)  # Use Broadcom pin numbering
-GPIO.setup(pin_number, GPIO.OUT)  # Set GPIO 18 as output
+GPIO.setup(pin_number, GPIO.OUT)  # Set GPIO pin_number as output
 
-def play_beep():
+while True:
+    print("Beep")
     GPIO.output(pin_number, True)  # Turn on the speaker
     time.sleep(1)  # Play the beep for 100ms
     GPIO.output(pin_number, False)  # Turn off the speaker
-
-play_beep()

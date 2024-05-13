8e3fd3a6ac128da9f37370845044374ca5142d81
Urix <43704209+uri-99@users.noreply.github.com>
2024-05-12 21:35:16 -0300

    fix: volume

--- app/src/alarm_test.py
+++ app/src/alarm_test.py
@@ -7,8 +7,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin_number, GPIO.OUT)
 
 tone = GPIO.PWM(pin_number, 440)
-volume = 100
-
+volume = 99
 
 notes = [262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523]
 tone_duration = 1
@@ -19,6 +18,7 @@ tone.start(volume)
 
 def play_a_tone(freq, duration):
     tone.ChangeFrequency(freq)
+    tone.ChangeDutyCycle(volume)
     time.sleep(duration)
 
 def play_a_rest(duration):

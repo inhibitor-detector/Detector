d80d7b7e9b1b8be6d302bb77d18c592bcfbbf779
Urix <43704209+uri-99@users.noreply.github.com>
2024-05-12 21:13:36 -0300

    feat(wip): speaker

--- app/src/alarm_test.py
+++ app/src/alarm_test.py
@@ -3,25 +3,31 @@ import RPi.GPIO as GPIO
 import board
 import pwmio
 
-tone = pwmio.PWMOut(board.GP15, variable_frequency=True)
-volume = 500
-tone.duty_cycle = volume
+tone = GPIO.PWM(board.GP15, 440)#variable_frequency=True)
+volume = 99
+# tone.duty_cycle = volume
 
-notes = [262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523]
-tone_duration = 0.5
-rest_duration = 0.1
+tone.start(volume)
 
-def play_a_tone(freq, duration):
-    tone.duty_cycle = volume
-    tone.frequency = freq
-    time.sleep(duration)
+time.sleep(10)
 
-def play_a_rest(duration):
-    tone.duty_cycle = 0
-    time.sleep(duration)
+tone.stop()
 
-for note in notes:
-    print("playing ", note)
-    play_a_tone(note, tone_duration)
-    play_a_rest(rest_duration)
+# notes = [262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523]
+# tone_duration = 0.5
+# rest_duration = 0.1
+
+# def play_a_tone(freq, duration):
+#     tone.duty_cycle = volume
+#     tone.frequency = freq
+#     time.sleep(duration)
+
+# def play_a_rest(duration):
+#     tone.duty_cycle = 0
+#     time.sleep(duration)
+
+# for note in notes:
+#     print("playing ", note)
+#     play_a_tone(note, tone_duration)
+#     play_a_rest(rest_duration)
     

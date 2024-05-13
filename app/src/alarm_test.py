f66390e9877ca519006168b5ecdec54f0d5a0bc6
Urix <43704209+uri-99@users.noreply.github.com>
2024-05-12 21:29:18 -0300

    feat: many notes

--- app/src/alarm_test.py
+++ app/src/alarm_test.py
@@ -1,38 +1,35 @@
 import time
 import RPi.GPIO as GPIO
-# import board
 
 pin_number = 8
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin_number, GPIO.OUT)
 
 tone = GPIO.PWM(pin_number, 440)
-volume = 1
-# tone.duty_cycle = volume
+volume = 100
+
+
+notes = [262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523]
+tone_duration = 0.5
+rest_duration = 0.1
+
 
 tone.start(volume)
 
-input('Press return to stop:')
+def play_a_tone(freq, duration):
+    tone.ChangeFrequency(freq)
+    time.sleep(duration)
+
+def play_a_rest(duration):
+    tone.duty_cycle = 0
+    time.sleep(duration)
+
+for note in notes:
+    print("playing ", note)
+    play_a_tone(note, tone_duration)
+    play_a_rest(rest_duration)
 
 tone.stop()
 GPIO.cleanup()
-
-# notes = [262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523]
-# tone_duration = 0.5
-# rest_duration = 0.1
-
-# def play_a_tone(freq, duration):
-#     tone.duty_cycle = volume
-#     tone.frequency = freq
-#     time.sleep(duration)
-
-# def play_a_rest(duration):
-#     tone.duty_cycle = 0
-#     time.sleep(duration)
-
-# for note in notes:
-#     print("playing ", note)
-#     play_a_tone(note, tone_duration)
-#     play_a_rest(rest_duration)
     
